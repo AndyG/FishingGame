de44488f4a6cebd514813ffb9afb91166a692bb5
thesisb <shadiisber@gmail.com>
2018-08-31 23:59:00 -0700

    Pause menu ctrl+v'd

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (GameIsPaused) {
+				Resume ();
+			} else {
+				Pause ();
+			}
+		}
+	}
+
+	void Resume () {
+		GameIsPaused = false;
+		// Set pausemenu game object to active
+		pauseMenuUI.SetActive (false);
+		// Freeze game time
+		Time.timeScale = 1f;
+	}
+
+	void Pause () {
+		GameIsPaused = true;
+		// Set pausemenu game object to active
+		pauseMenuUI.SetActive (true);
+		// Freeze game time
+		Time.timeScale = 0f;
+	}
+}
